7e80c545d25c87649274b5874c28870ab1a75908
Govindarajan Nagarajan <govindarajan.nagarajan@wolt.com>
2022-12-01 23:23:15 +0100

    Add Unary Plus and Minus

--- test.sh
+++ test.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 zig build || exit
-./zig-out/bin/chibicc-zig "(33+ 44) - 7-10" > tmp.s || exit
-as -arch arm64 -o tmp.asm tmp.s
-ld -o tmp tmp.asm -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64
-./tmp
-actual="$?"
+assert() {
+  expected="$1"
+  input="$2"
 
-if [ "$actual" = "60" ]; then
- echo "60 => $actual"
-else
-  echo "7 expected, but got $actual"
-  exit 1
-fi
+  ./zig-out/bin/chibicc-zig "$input" > tmp.s || exit
+  as -arch arm64 -o tmp.asm tmp.s
+  ld -o tmp tmp.asm -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64
+  ./tmp
+  actual="$?"
 
-echo OK
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+assert 21 '5+20-4'
+assert 41 ' 12 + 34 - 5 '
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
+assert 10 '-10+20'
+assert 10 '- -10'
+assert 10 '- - +10'
+
+echo OK 
