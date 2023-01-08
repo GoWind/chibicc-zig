8208bc01861a358939d75bb569a120e82bdff2c6
Govind <petrovgovind@gmail.com>
2023-01-08 02:16:10 +0100

    Add a tokenizer to allow space characters between tokens

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 zig build || exit
-./zig-out/bin/chibicc-zig "3" "+" "4" > tmp.s || exit
+./zig-out/bin/chibicc-zig "33+ 44 - 7-10" > tmp.s || exit
 as -arch arm64 -o tmp.asm tmp.s
 ld -o tmp tmp.asm -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64
 ./tmp
 actual="$?"
 
-if [ "$actual" = "7" ]; then
- echo "7 => $actual"
+if [ "$actual" = "60" ]; then
+ echo "60 => $actual"
 else
   echo "7 expected, but got $actual"
   exit 1
